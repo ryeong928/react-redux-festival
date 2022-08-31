16f53b8a2645cba6e68ad9a5c49c0e0e120b6718
feryeong <feryeong@nc-and.com>
2022-08-31 17:46:23 +0900

    saga!

--- courses/course4.js
+++ courses/course4.js
@@ -3,9 +3,9 @@ redux : a predictable state container for javascript applications
   -> createStore, subscribe, getState, dispatch
 react-redux : a library that provides bindings to use React and Redux together in an application. it allows react to use redux store
   -> connect, useDispatch, useSelector
-redux-thunk : 
+redux-thunk : 함수를 만들어서 비동기 작업을 하고 필요한 시점에 특정 액션을 디스패치한다
 
-redux-saga : 
+redux-saga : 특정 액션을 모니터링하고, 해당 액션이 발동되면 제터레이터 함수를 실행하여 비동기 작업을 처리 후 액션을 디스패치
 
 redux-toolkit : a library for efficient redux dev
   -> configureStore, createSlice, createAsyncThunk
